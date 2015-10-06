d4a06e0fb11bf4fbe34f5a08fc1c9d9ba2f13b22
Ouraios <ouraiios@gmail.com>
2015-10-06 12:14:40 +0200

    Modification menu

--- js/demo1.js
+++ js/demo1.js
@@ -3,11 +3,17 @@
 	var button = document.getElementById('cn-button'),
     wrapper = document.getElementById('cn-wrapper'),
     overlay = document.getElementById('cn-overlay');
+    icones  = document.getElementsByClassName('icones');
 
 	//open and close menu when the button is clicked
 	var open = false;
 	button.addEventListener('click', handler, false);
 	wrapper.addEventListener('click', cnhandle, false);
+	icones[0].addEventListener('click', closeNav);
+	icones[1].addEventListener('click', closeNav);
+	icones[2].addEventListener('click', closeNav);
+	icones[3].addEventListener('click', closeNav);
+	icones[4].addEventListener('click', closeNav);
 
 	function cnhandle(e){
 		e.stopPropagation();
